608051c0e35384baa1e07dac2657c1e48212f04e
Lucas Xavier <lucas.lima.xaviier@gmail.com>
2023-10-27 10:44:26 -0400

    User insert with POST method

--- src/main/java/com/lucasxavier/springbootmongo/services/UserService.java
+++ src/main/java/com/lucasxavier/springbootmongo/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.lucasxavier.springbootmongo.dto.UserDTO;
 import com.lucasxavier.springbootmongo.entities.User;
 import com.lucasxavier.springbootmongo.repositories.UserRepository;
 import com.lucasxavier.springbootmongo.services.exception.ObjectNotFoundException;
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
