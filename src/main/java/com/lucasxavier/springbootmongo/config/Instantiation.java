fb0e75800c870cadb13f81a9d6180d819ab79722
Lucas Xavier <lucas.lima.xaviier@gmail.com>
2023-11-06 10:30:41 -0400

    Adding comments to Post

--- src/main/java/com/lucasxavier/springbootmongo/config/Instantiation.java
+++ src/main/java/com/lucasxavier/springbootmongo/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.lucasxavier.springbootmongo.dto.AuthorDTO;
+import com.lucasxavier.springbootmongo.dto.CommentDTO;
 import com.lucasxavier.springbootmongo.entities.Post;
 import com.lucasxavier.springbootmongo.entities.User;
 import com.lucasxavier.springbootmongo.repositories.PostRepository;
@@ -42,7 +43,14 @@ public class Instantiation implements CommandLineRunner {
 	 	Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 	 	Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 	 
-		postRepository.saveAll(Arrays.asList(post1,post2));
+	 	CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().add(c3);
+	 	
+	 	postRepository.saveAll(Arrays.asList(post1,post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1,post2));
 		userRepository.save(maria);
