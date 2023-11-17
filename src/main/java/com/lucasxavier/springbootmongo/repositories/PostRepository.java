f10082921f439e418dc12db1466f848a12772014
Lucas Xavier <lucas.lima.xaviier@gmail.com>
2023-11-17 10:39:52 -0400

    Simple consult with annotation @Query

--- src/main/java/com/lucasxavier/springbootmongo/repositories/PostRepository.java
+++ src/main/java/com/lucasxavier/springbootmongo/repositories/PostRepository.java
@@ -3,10 +3,14 @@ package com.lucasxavier.springbootmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.lucasxavier.springbootmongo.entities.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String Text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
